6ffe65fc1933f97c5984000bae335c902b92bd19
vladward <vladward13@gmail.com>
2021-10-25 15:47:17 +0300

    feat: first homework

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import s from "./Message.module.css"
+
+export type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={s.message}>
+            <img src={props.avatar} alt="avatar" className={s.avatar}/>
+
+            <div className={s.angle}></div>
+
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
