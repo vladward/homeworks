425507ff0b5479a1c60bc4b8297d46b4b7167796
vladward <vladward13@gmail.com>
2021-10-31 11:24:53 +0300

    ref: refactor Greeting component

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string
+    setNameCallback: (title: string) => void
+    addUserCallBack: () => void
+    error: string
+    totalUsers: number
 }
 
-// презентационная компонента (для верстальщика)
-const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
-) => {
-    const inputClass = s.error // need to fix with (?:)
+const Greeting = (props: GreetingPropsType) => {
+    const inputClass = props.error.length ? s.error : ''
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.setNameCallback(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.charCode === 13) {
+            sendWithEnter()
+        }
+    }
+
+    const sendWithEnter = () => {
+        props.addUserCallBack()
+    }
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <input type="input"
+                   value={props.name}
+                   onChange={onChangeHandler}
+                   className={inputClass}
+                   onKeyPress={onKeyPressHandler}
+            />
+            <button disabled={!!props.error.length} onClick={sendWithEnter}>add</button>
+            <span>{props.totalUsers}</span>
+            <div>
+                <span className={inputClass}>{props.error}</span>
+            </div>
+
         </div>
     )
 }
