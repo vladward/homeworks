3d623921bebfa5eb3eadf968b6f7cee6b486b370
vladward <vladward13@gmail.com>
2021-10-31 13:03:22 +0300

    HW2ref: refactor data type

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -4,7 +4,7 @@ import {AffairType, FilterType} from './HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any
+    data: AffairType[]
     changeFilter: (filter: FilterType) => void
     deleteAffairCallback: (_id: string) => void
 }
