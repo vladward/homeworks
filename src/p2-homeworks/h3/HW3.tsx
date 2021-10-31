5c21e0c60d0cc2d0f45a74bd66014bdf3d122843
vladward <vladward13@gmail.com>
2021-10-31 11:23:10 +0300

    ref: refactor hw3 component

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,27 +1,30 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from 'uuid';
 
-// types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string
+    name: string
 }
 
-// уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([])
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string) => {
+        let user = {_id: v1(), name: name}
+        let newUser = [...users, user]
+
+        setUsers(newUser)
     }
+    let totalUsers = users.length
+    console.log(users)
 
     return (
         <div>
             <hr/>
             homeworks 3
 
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={users} addUserCallback={addUserCallback} totalUsers={totalUsers}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
