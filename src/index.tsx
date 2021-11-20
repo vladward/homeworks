b2a9cdbccc0453190155886451d23c38da6063bc
vladward <vladward13@gmail.com>
2021-11-20 17:55:23 +0300

    fix: clean unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, HashRouter, Routes} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
