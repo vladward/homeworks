a4411707f965fa3d5c340931e4c48a3b556e0cd1
vladward <vladward13@gmail.com>
2021-11-20 17:56:16 +0300

    ref: refactor 404 page

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import s from './../../../p1-main/m1-ui/u1-app/App.module.css'
+import {Link} from "react-router-dom";
+import {PATH} from "../MyRoutes";
 
 function Error404() {
+
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.error}>
+            <div className={s.error_text}/>
+            <div className={s.error_text2}><h1>404</h1>
+                <h3>Page not found!</h3>
+                <Link to={PATH.PRE_JUNIOR}>Go Home</Link>
+            </div>
         </div>
     )
 }
