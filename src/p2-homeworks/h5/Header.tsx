ac6fcf3bac20c207889190e21b21a051f334637f
vladward <vladward13@gmail.com>
2021-11-20 12:50:12 +0300

    feat: add buttons for pages

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {PATH} from "./MyRoutes"
+import s from './../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.headerLinks}>
+            <button><Link to={PATH.PRE_JUNIOR}>Pre-Junior</Link></button>
+            <button><Link to={PATH.JUNIOR}>Junior</Link></button>
+            <button><Link to={PATH.JUNIOR_PLUS}>Junior-Plus</Link></button>
         </div>
     )
 }
