815722f645f8965721a006fcd7d1f7ab23be9e0e
vladward <vladward13@gmail.com>
2021-11-20 17:54:30 +0300

    fix: 404 page fix

--- src/p2-homeworks/h5/MyRoutes.tsx
+++ src/p2-homeworks/h5/MyRoutes.tsx
@@ -10,24 +10,18 @@ export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
     JUNIOR_PLUS: '/junior-plus'
-    // add paths
 }
 
 function MyRoutes() {
     return (
         <Routes>
-            {/*Switch выбирает первый подходящий роут*/}
-
-            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
             <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
             <Route path={PATH.JUNIOR} element={ <Junior/>}/>
             <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                {/*// add routes*/}
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            <Route element={<Error404/>}/>
+
+            <Route path={'*'} element={<Error404/>}/>
         </Routes>
     )
 }
