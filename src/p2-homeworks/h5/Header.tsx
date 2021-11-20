585bd5d0218fd322750fcecdbde321af75c24862
vladward <vladward13@gmail.com>
2021-11-20 17:56:03 +0300

    ref: refactor header component links

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import {PATH} from "./MyRoutes"
 import s from './../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
+
     return (
         <div className={s.headerLinks}>
-            <button><Link to={PATH.PRE_JUNIOR}>Pre-Junior</Link></button>
-            <button><Link to={PATH.JUNIOR}>Junior</Link></button>
-            <button><Link to={PATH.JUNIOR_PLUS}>Junior-Plus</Link></button>
+            <div className={s.headerLinks_container}>
+                <Link className={s.button1} to={PATH.PRE_JUNIOR}>Pre-Junior</Link>
+                <Link className={s.button2} to={PATH.JUNIOR}>Junior</Link>
+                <Link className={s.button3} to={PATH.JUNIOR_PLUS}>Junior-Plus</Link>
+                <div className={s.coub}></div>
+            </div>
         </div>
     )
 }
