b5f1e617dc6d162e0029d8bfc439b3064ddc0b00
vladward <vladward13@gmail.com>
2021-12-04 23:02:11 +0300

    feat: uncomment hw8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
 
 export const Junior = () => {
     return <div>
         <HW7/>
+        <HW8/>
     </div>
 }
