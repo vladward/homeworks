04ec8d1d58bbc57aef4d3f7e8b7560a53f90df22
vladward <vladward13@gmail.com>
2021-12-05 00:32:43 +0300

    feat: create homeWorkReducer for HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,38 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+const initialPeople: UserType[] = [
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
+
+export const homeWorkReducer = (state = initialPeople, action: SortReducerActionTypes): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': {
+        case 'SORT-UP': {
             // need to fix
-            return state
+            let copy = [...state]
+            return copy.sort((a, b) => b.age - a.age)
         }
-        case 'check': {
+        case 'SORT-DAWN': {
             // need to fix
-            return state
+            let copy = [...state]
+            return copy.sort((a, b) => b.age - a.age).reverse()
         }
-        default: return state
+        case 'SORT-AGE': {
+            // need to fix
+            return state.filter(c => c.age > action.payload).sort((a, b) => a.age - b.age)
+        }
+        default:
+            return state
     }
 }
+type SortReducerActionTypes = sortUpType | sortDawnType | sortAgeType
+type sortUpType = ReturnType<typeof SortUpAC>
+export const SortUpAC = () => {return {type: 'SORT-UP'} as const}
+type sortDawnType = ReturnType<typeof SortDawnAC>
+export const SortDawnAC = () => {return {type: 'SORT-DAWN'} as const}
+type sortAgeType = ReturnType<typeof SortAgeAC>
+export const SortAgeAC = (payload: number) => {return {type: 'SORT-AGE', payload} as const}
