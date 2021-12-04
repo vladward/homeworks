4e9fe5e8d8c3e4d72eae096940f8595dc9b31140
vladward <vladward13@gmail.com>
2021-12-05 00:33:19 +0300

    feat: HW8 logic create

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,42 +1,48 @@
 import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import {homeWorkReducer, SortAgeAC, SortDawnAC, SortUpAC} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import s from './styles.module.css'
+import {useDispatch, useSelector} from "react-redux";
+import {rootReducerType} from "./bll/store";
 
-// export type UserType =
-
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+}
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
 
+    let users = useSelector<rootReducerType, Array<UserType>>(state => state.users);
+
+    let dispatch = useDispatch()
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = users.map((p, i) => (
+        <div className={s.main} key={i}>
+            <div>{p.name}</div>
+            <div>{p.age}</div>
         </div>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortUp = () => dispatch({type: 'SORT-UP'})
+    const sortDawn = () => dispatch({type: 'SORT-DAWN'})
+    const sortAge = (payload: number) => dispatch({type: 'SORT-AGE', payload})
 
     return (
-        <div className={s.main}>
-            <hr/>
+        <div>
             homeworks 8
+            <hr/>
+            <div className={s.wrapper}>
 
-            {/*should work (должно работать)*/}
-            {finalPeople}
+                {/*should work (должно работать)*/}
+                {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+                <div className={s.buttons}>
+                    <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+                    <div><SuperButton onClick={sortDawn}>sort dawn</SuperButton></div>
+                    <div><SuperButton onClick={() => sortAge(18)}>sort age</SuperButton></div>
+                </div>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
