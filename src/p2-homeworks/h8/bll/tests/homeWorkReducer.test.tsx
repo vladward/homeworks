db7a806a0cb65cf9fe3e78866981c3c96247ca23
vladward <vladward13@gmail.com>
2021-12-05 00:31:43 +0300

    feat: add tests for HW8

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
+import {UserType} from "../../HW8";
 
-let initialState: any[] // need to fix any
+let initialState: UserType[] // need to fix any
 
 beforeEach(() => {
     initialState = [
@@ -15,18 +16,29 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const newState = homeWorkReducer(initialState, {type: 'SORT-UP'})
 
+
+     expect(newState[0].name).toBe('Александр')
+     expect(newState[1].name).toBe('Ирина')
+     expect(newState.length).toBe(6)
     console.log(newState)
-    // expect(...).toBe(...)
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    const newState = homeWorkReducer(initialState, {type: 'SORT-DAWN'})
 
-
+    expect(newState[0].name).toBe('Кот')
+    expect(newState[1].name).toBe('Коля')
+    expect(newState.length).toBe(6)
+    console.log(newState)
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+    const newState = homeWorkReducer(initialState, {type: 'SORT-AGE', payload: 18})
 
-
+    expect(newState[0].name).toBe('Дмитрий')
+    expect(newState[0].age).toBe(40)
+    expect(newState[1].name).toBe('Виктор')
+    expect(newState[1].age).toBe(44)
+    expect(newState.length).toBe(4)
+    console.log(newState)
 })
