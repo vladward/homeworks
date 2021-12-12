f2ce30c370472865ad0d095c7030179566210dfd
vladward <vladward13@gmail.com>
2021-12-12 19:44:03 +0300

    feat: add styles & logic for Clock

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,50 +1,57 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import moment from "moment";
+import s from "../../p1-main/m1-ui/u1-app/App.module.css"
+
 
 function Clock() {
+    const stringTime = moment().format('HH:mm:ss') // fix with date
+    const stringDate = moment().format('DD.MM.YYYY') // fix with date
+
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState('1')
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
-        stop()
+        //stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(stringTime)
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
-
-            {show && (
-                <div>
-                    {stringDate}
+            <div className={s.container}>
+                <div className={s.container_time}>
+                    <div className={s.button_box}>
+                        <SuperButton onClick={start}>start</SuperButton>
+                    </div>
+                    <div
+                        onMouseEnter={onMouseEnter}
+                        onMouseLeave={onMouseLeave}
+                        className={s.container_date}
+                    >
+                        <label className={s.label}>{stringTime}</label>
+                        {show && (
+                            <label>{stringDate}</label>
+                        )}
+                    </div>
+                    <div className={s.button_box}>
+                        <SuperButton className={s.button_box_button} onClick={stop}>stop</SuperButton>
+                    </div>
                 </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            </div>
         </div>
     )
 }
