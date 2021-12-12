fdd21d52a25b7fe976d76ed12fdecb086bd34fd7
vladward <vladward13@gmail.com>
2021-12-12 16:50:33 +0300

    feat: install moment js

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 export const Junior = () => {
     return <div>
         <HW7/>
         <HW8/>
+        <HW9/>
     </div>
 }
