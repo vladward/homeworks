418b86a521d42fe6658d2632823b2a8f2e68b47c
vladward <vladward13@gmail.com>
2021-12-19 12:48:19 +0300

    ref: refactor date format with native functions

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -5,20 +5,17 @@ import s from "../../p1-main/m1-ui/u1-app/App.module.css"
 
 
 function Clock() {
-    const stringTime = moment().format('HH:mm:ss') // fix with date
-    const stringDate = moment().format('DD.MM.YYYY') // fix with date
-
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState('1')
+    const [date, setDate] = useState<Date>()
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         clearInterval(timerId)
     }
     const start = () => {
-        //stop()
-        const id: number = window.setInterval(() => {
-            setDate(stringTime)
+        stop()
+        const id: number = +setInterval(() => {
+            setDate(new Date)
         }, 1000)
         setTimerId(id)
     }
@@ -29,6 +26,11 @@ function Clock() {
     const onMouseLeave = () => {
         setShow(false)
     }
+
+    // const stringTime = moment().format('HH:mm:ss') // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/>
+    // const stringDate = moment().format('DD.MM.YYYY') // fix with date
+    const stringDate = date?.toLocaleDateString()
 
     return (
         <div>
