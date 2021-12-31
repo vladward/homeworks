2f757663b650a9e76c38477aa67608e4d7c447db
vladward <vladward13@gmail.com>
2021-12-31 17:52:59 +0300

    add reducer

--- src/p2-homeworks/h8/bll/store.ts
+++ src/p2-homeworks/h8/bll/store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import {homeWorkReducer} from "./homeWorkReducer";
 import {loadingReducer} from "../../h10/bll/loadingReducer";
+import {themeReducer} from "../../h12/bll/themeReducer";
 
 let rootReducer = combineReducers({
     users: homeWorkReducer,
     loading: loadingReducer,
+    theme: themeReducer
 })
 
 export type rootReducerType = ReturnType<typeof rootReducer>
