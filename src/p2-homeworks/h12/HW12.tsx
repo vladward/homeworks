f6dd1f54234696695221e4f27ac4df962ac4ecdb
vladward <vladward13@gmail.com>
2021-12-31 17:55:31 +0300

    add useDispatch and useSelector in component

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {rootReducerType} from "../h8/bll/store";
+import {ChangeThemeC, ColorThemeType} from "./bll/themeReducer";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
-
+    const theme = useSelector<rootReducerType, ColorThemeType>(state => state.theme)
+    let dispatch = useDispatch()
+    const onChangeCallBack = (value: string) => {
+        dispatch(ChangeThemeC(value))
+    }
+    console.log(theme.colorTheme + '-text')
     return (
         <div className={s[theme]}>
             <hr/>
