abd6aa306adf829a50faaf638ee0c001b7b38545
vladward <vladward13@gmail.com>
2021-12-31 17:54:14 +0300

    uncomment reducer for loading

--- src/p2-homeworks/h10/bll/store.ts
+++ src/p2-homeworks/h10/bll/store.ts
@@ -1,13 +1,13 @@
 import {loadingReducer} from './loadingReducer'
 import {combineReducers, createStore} from "redux";
 
-// export const reducers = combineReducers({
-//     //loading: loadingReducer,
-// })
-//
-// export const store = createStore(reducers)
-//
-// export type AppStoreType = ReturnType<typeof reducers>
-//
-// // @ts-ignore
-// window.store = store // for dev
+ export const reducers = combineReducers({
+     loading: loadingReducer,
+ })
+
+ export const store = createStore(reducers)
+
+ export type AppStoreType = ReturnType<typeof reducers>
+
+ // @ts-ignore
+ window.store = store // for dev
