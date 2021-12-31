1176c6b8b4b88428d78c110f4c626f9c8ea2f278
vladward <vladward13@gmail.com>
2021-12-31 17:24:20 +0300

    double range logic refactor

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import {Slider, Typography} from "@material-ui/core";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    onChangeRange?: (value: number[]) => void
+    value?: number[]
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        onChangeRange, value, ...restProps
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    const onChangeCallback = (e: ChangeEvent<{}>, newValue: number | number[]) => {
+        if (value) {
+            onChangeRange && onChangeRange(newValue as number[])
+        }
+    }
+
+    function valuetext(value: number) {
+        return `${value}`;
+    }
+
 
     return (
         <>
-            DoubleRange
+            {value !== null && <div>
+                <Typography id="range-slider" gutterBottom>
+                     range
+                </Typography>
+                <Slider
+                    style={{width: '250px'}}
+                    value={value}
+                    min={0}
+                    max={100}
+                    step={1}
+                    // scale={x => x ** 10}
+                    onChange={onChangeCallback}
+                    valueLabelDisplay="off"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                />
+            </div>}
         </>
     )
 }
