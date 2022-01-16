80daf8e46f36edc49ea86ce8937abea2cb2f6d61
vladward <vladward13@gmail.com>
2022-01-16 14:53:33 +0300

    feat: HW13 enable

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
 import Header from './Header'
 import MyRoutes from './MyRoutes'
 
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,8 +1,10 @@
 import HW12 from "../../h12/HW12";
+import {HW13} from "../../h13/HW13";
 
 
 export const JuniorPlus = () => {
     return <div>
         <HW12/>
+        <HW13/>
     </div>
 }
